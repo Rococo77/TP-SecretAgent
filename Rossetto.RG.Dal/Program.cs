a92a807032b6a3c05d424b0013a24702cbed692a
CORENTIN ROSSETTO <114078357+Rococo77@users.noreply.github.com>
2024-04-19 13:50:20 +0200

    init

---
+++ Rossetto.RG.Dal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rossetto.RG.Dal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
